12f7c5d3ea391794939857cbec07ad70b458fa6e
MBanawa <mikebanawa25@gmail.com>
2020-09-05 08:58:38 +0800

    commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# dice_app
+
+A Stateful Dice Rolling Flutter application.
+
+## Features
+
+A super simple Dice Rolling app where when you click on any die, it's gonna change both dice faces.
+You're gonna be able to use this to settle any scores you have in real-life such as, who will wash the dishes?
---
+++ lib/main.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.deepPurple,
+        appBar: AppBar(
+          backgroundColor: Colors.deepPurple,
+          title: Text(
+            'Super Dicer',
+          ),
+        ),
+        body: DicePage(),
+      ),
+    ),
+  );
+}
+
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
+
+  void randomDice() {
+    setState(() {
+      leftDiceNumber = Random().nextInt(6) + 1;
+      rightDiceNumber = Random().nextInt(6) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              child: Image.asset('images/dice$leftDiceNumber.png'),
+              onPressed: () {
+                randomDice();
+              },
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+              child: Image.asset('images/dice$rightDiceNumber.png'),
+              onPressed: () {
+                randomDice();
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
